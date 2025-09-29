a4d6ca00aeed75e909080fb756db3fb5dcdd7d28
echo <hujw77@gmail.com>
2025-09-29 12:44:25 +0800

    tx

--- utils/wallets/private_key.go
+++ utils/wallets/private_key.go
@@ -189,7 +189,7 @@ func (p *PrivateKeyWallet) SendTransaction(ctx context.Context, tx *types.Dynami
 	}
 
 	if tx.GasTipCap == nil {
-		tip := new(big.Int).Div(tx.GasFeeCap, big.NewInt(20))
+		tip := big.Int.from(200000000)
 		logger.Infof("gas tip: %s", tip.String())
 		tx.GasTipCap = tip
 	}
